22b89f2aa6d2fe4b9f120560957abcaac05893b5
ko2ic <ko2ic.dev@gmail.com>
2020-10-25 01:11:48 +0900

    add RecyclerView

--- app/src/main/java/com/ko2ic/coroutinesflow/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ko2ic/coroutinesflow/ui/home/HomeFragment.kt
@@ -8,24 +8,78 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ko2ic.coroutinesflow.R
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+
+        viewManager = LinearLayoutManager(activity)
+
+        homeViewModel.list.observe(viewLifecycleOwner, Observer {
+            viewAdapter = MyAdapter(it)
+            recyclerView = root.findViewById<RecyclerView>(R.id.recyclerView).apply {
+                // use this setting to improve performance if you know that changes
+                // in content do not change the layout size of the RecyclerView
+                setHasFixedSize(true)
+
+                // use a linear layout manager
+                layoutManager = viewManager
+
+                // specify an viewAdapter (see also next example)
+                adapter = viewAdapter
+
+            }
         })
+
         return root
+    }
+
+    class MyAdapter(private val myDataset: List<Comment>) :
+        RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder.
+        // Each data item is just a string in this case that is shown in a TextView.
+        class MyViewHolder(val viewGroup: ViewGroup) : RecyclerView.ViewHolder(viewGroup)
+
+        // Create new views (invoked by the layout manager)
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): MyAdapter.MyViewHolder {
+            // create a new view
+            val viewGroup = LayoutInflater.from(parent.context)
+                .inflate(R.layout.adapter_item, parent, false) as ViewGroup
+            // set the view's size, margins, paddings and layout parameters
+            return MyViewHolder(viewGroup)
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.viewGroup.findViewById<TextView>(R.id.body).text =
+                myDataset[position].body
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        override fun getItemCount() = myDataset.size
     }
 }
--- app/src/main/java/com/ko2ic/coroutinesflow/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/ko2ic/coroutinesflow/ui/home/HomeViewModel.kt
@@ -6,8 +6,21 @@ import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _list = MutableLiveData<List<Comment>>().apply {
+        value = listOf(
+            Comment(1, 1, "name1-1", "name1-1@gmail.com", "body1-1"),
+            Comment(1, 2, "name1-2", "name1-2@gmail.com", "body1-2"),
+            Comment(1, 3, "name1-3", "name1-3@gmail.com", "body1-3")
+        )
     }
-    val text: LiveData<String> = _text
+    val list: LiveData<List<Comment>> = _list
 }
+
+data class Comment(
+    val postId: Long,
+
+    val id: Long,
+    val name: String,
+    val email: String,
+    val body: String
+)
