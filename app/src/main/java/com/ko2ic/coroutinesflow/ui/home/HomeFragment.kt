74513b4a176d20a48a7d3b446ecbb887113f2dfe
ko2ic <ko2ic.dev@gmail.com>
2020-10-31 01:28:14 +0900

    add databinding

--- app/src/main/java/com/ko2ic/coroutinesflow/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ko2ic/coroutinesflow/ui/home/HomeFragment.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ko2ic.coroutinesflow.R
+import com.ko2ic.coroutinesflow.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
@@ -27,27 +29,22 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(
+            inflater, R.layout.fragment_home, container, false
+        )
+
+        binding.viewModel = homeViewModel
 
         viewManager = LinearLayoutManager(activity)
 
         homeViewModel.list.observe(viewLifecycleOwner, Observer {
-            viewAdapter = MyAdapter(it)
-            recyclerView = root.findViewById<RecyclerView>(R.id.recyclerView).apply {
-                // use this setting to improve performance if you know that changes
-                // in content do not change the layout size of the RecyclerView
-                setHasFixedSize(true)
-
-                // use a linear layout manager
-                layoutManager = viewManager
-
-                // specify an viewAdapter (see also next example)
-                adapter = viewAdapter
-
-            }
+            binding.recyclerView.adapter = MyAdapter(it)
         })
 
-        return root
+        val view = binding.root
+
+        return view
     }
 
     class MyAdapter(private val myDataset: List<Comment>) :
